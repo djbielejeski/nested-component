da93ab50eecb61e44f1c72a06aad776d84d699f5
David Bielejeski <djbielejeski@gmail.com>
2018-09-22 15:38:47 -0500

    Stubbing out the service to fetch its children's options.

--- src/app/shared/components/nested.component.ts
+++ src/app/shared/components/nested.component.ts
@@ -1,16 +1,29 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 import { ArtifactOption } from '../models';
+import {ArtifactsOptionsService} from '../services/artifacts-options.service';
 
 @Component({
   selector: 'app-nested-component',
   templateUrl: './nested.component.html',
   styleUrls: ['./nested.component.css']
 })
-export class NestedComponent {
+export class NestedComponent implements OnInit {
   @Input() data: ArtifactOption = null;
   @Output() dataSelected = new EventEmitter<ArtifactOption>();
 
   selectedOption: ArtifactOption  = null;
   selectedOptions: ArtifactOption[] = [];
-  expanded = false;
+
+  constructor(private artifactsOptionsService: ArtifactsOptionsService) {}
+
+  ngOnInit() {
+    // Fetch yourself from the server
+    // TODO: Alex
+
+    // Pass in the parent id to the server to fetch all the sub-properties the user will be able to set
+    // this.artifactsOptionsService.getOptionsByParentId(data.id).subscribe((properties: ArtifactOption[])=> {
+    //  // Once you assign the sub properties, they will all render and render their available children.
+    //  this.data.properties = properties;
+    // };
+  }
 }
