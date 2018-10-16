460f7f37c40f8670cf820a99f50076b0ff5ef4c6
djbielejeski <djbielejeski@gmail.com>
2018-10-16 09:49:06 -0500

    Fixing typescript redsquigglies

--- src/app/shared/models/selected-artifact.model.ts
+++ src/app/shared/models/selected-artifact.model.ts
@@ -1,6 +1,6 @@
 import { ArtifactOption } from './artifact-option.model';
 
 export interface SelectedArtifactModel {
-  selected: ArtifactOption | ArtifactOption[] | SelectedArtifactModel;
+  selected: any; // will be one of these types: ArtifactOption | ArtifactOption[] | SelectedArtifactModel
   parent: ArtifactOption;
 }
